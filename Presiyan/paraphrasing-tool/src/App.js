c0b097610ecde51787404f1fe9b3b8e61c525ed2
mars-xion <maz200027@gmail.com>
2024-01-24 15:45:00 +0100

    modifed the css style

--- Presiyan/paraphrasing-tool/src/App.js
+++ Presiyan/paraphrasing-tool/src/App.js
@@ -1,38 +1,39 @@
-import React, { useState } from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import Divider from '@material-ui/core/Divider';
-import TextField from '@material-ui/core/TextField';
-import Container from '@material-ui/core/Container';
-import Header from './Header';
-import Sidebar from './Sidebar';
+import React, { useState } from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Divider from "@material-ui/core/Divider";
+import TextField from "@material-ui/core/TextField";
+import Container from "@material-ui/core/Container";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
 
-import SubmitButton from './Submit/SubmitButton';
+import SubmitButton from "./Submit/SubmitButton";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    flexDirection: 'column',
-    minHeight: '100vh',
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "100vh",
+    backgroundColor: "white",
   },
   paraphrasedText: {
-    wordWrap: 'break-word',
-    whiteSpace: 'pre-wrap',
+    wordWrap: "break-word",
+    whiteSpace: "pre-wrap",
   },
   gridRoot: {
     flexGrow: 1,
   },
   formRoot: {
-    '& > *': {
+    "& > *": {
       margin: theme.spacing(1),
-      width: '100%',
+      width: "100%",
     },
   },
   primary: {
-    '& > *': {
+    "& > *": {
       margin: theme.spacing(1),
     },
   },
@@ -42,7 +43,7 @@ const useStyles = makeStyles((theme) => ({
   },
   paper: {
     padding: theme.spacing(2),
-    textAlign: 'center',
+    textAlign: "center",
     color: theme.palette.text.secondary,
   },
 }));
@@ -50,11 +51,13 @@ const useStyles = makeStyles((theme) => ({
 export default function ParaphrasingTool() {
   const classes = useStyles();
 
-  const [inputText, setInputText] = useState('')
-  const [apiResponseText, setApiResponseText] = useState('Nothing here yet, enter some text in the box above')
+  const [inputText, setInputText] = useState("");
+  const [apiResponseText, setApiResponseText] = useState(
+    "Nothing here yet, enter some text in the box above"
+  );
 
   function setResponseText(apiResponse) {
-    setApiResponseText(apiResponse)
+    setApiResponseText(apiResponse);
   }
 
   return (
@@ -67,55 +70,60 @@ export default function ParaphrasingTool() {
           Paraphrasing Tool
         </Typography>
         <Typography variant="h5" component="h2" gutterBottom>
-          {' Rewrite any text using AI.'}
-          {''}
+          {" Rewrite any text using AI."}
+          {""}
         </Typography>
 
         <div className={classes.gridRoot}>
           <Grid container spacing={3}>
-            <Grid item xs={12}>
-            </Grid>
+            <Grid item xs={12}></Grid>
             <Grid item xs={12}>
               <Paper elevation={3} className={classes.paper}>
-
-                <form className={classes.formRoot} noValidate autoComplete="off">
-                <TextField
-                  id="standard-basic"
-                  label="Insert the text you would like to rewrite here"
-                  placeholder="I love it when AI does all the work for me"
-                  multiline
-                  rows={4} 
-                  variant="outlined"
-                  fullWidth 
-                  onChange={(e) => setInputText(e.target.value)}
-                />
+                <form
+                  className={classes.formRoot}
+                  noValidate
+                  autoComplete="off"
+                >
+                  <TextField
+                    id="standard-basic"
+                    label="Insert the text you would like to rewrite here"
+                    placeholder="I love it when AI does all the work for me"
+                    multiline
+                    rows={4}
+                    variant="outlined"
+                    fullWidth
+                    onChange={(e) => setInputText(e.target.value)}
+                  />
                 </form>
-
               </Paper>
             </Grid>
-            <Grid item xs={5}>
-            </Grid>
+            <Grid item xs={5}></Grid>
             <Grid item xs={2}>
               <div className={classes.primary}>
-                <SubmitButton inputText={inputText} responseText={setResponseText} />
+                <SubmitButton
+                  inputText={inputText}
+                  responseText={setResponseText}
+                />
               </div>
             </Grid>
-            <Grid item xs={5}>
-            </Grid>
+            <Grid item xs={5}></Grid>
             <Grid item xs={12}>
-        <Paper elevation={3} className={classes.paper}>
-          <Typography variant="h6" style={{ fontWeight: 700, marginBottom: '10px' }}> New Text: </Typography>
-          <Divider variant="middle" />
-          <Typography variant="h6" className={classes.paraphrasedText}> 
-            {apiResponseText} 
-          </Typography>
-        </Paper>
-      </Grid>
-
+              <Paper elevation={3} className={classes.paper}>
+                <Typography
+                  variant="h6"
+                  style={{ fontWeight: 700, marginBottom: "10px" }}
+                >
+                  {" "}
+                  New Text:{" "}
+                </Typography>
+                <Divider variant="middle" />
+                <Typography variant="h6" className={classes.paraphrasedText}>
+                  {apiResponseText}
+                </Typography>
+              </Paper>
+            </Grid>
           </Grid>
         </div>
-
-
       </Container>
     </div>
   );
--- Presiyan/paraphrasing-tool/src/Sidebar.js
+++ Presiyan/paraphrasing-tool/src/Sidebar.js
@@ -1,52 +1,52 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   sidebar: {
-    border: '3px solid #e7e7e7',
-    backgroundColor: '#fff',
+    border: "3px solid #e7e7e7",
+    backgroundColor: "#fff",
     borderRadius: 7,
-    width: 60, // Increased to accommodate icon + padding
-    height: '30vh',
-    position: 'fixed',
-    padding: '20px 0', // Top and bottom padding only
-    left: '5%',
-    top: '20%',
-    transition: 'width 0.3s ease-in-out',
-    overflow: 'hidden', // Prevents content from overflowing
-    '&:hover': {
+    width: 50, // Increased to accommodate icon + padding
+    height: "25vh",
+    position: "fixed",
+    padding: "20px 0", // Top and bottom padding only
+    left: "5%",
+    top: "20%",
+    transition: "width 0.3s ease-in-out",
+    overflow: "hidden", // Prevents content from overflowing
+    "&:hover": {
       width: 200, // Width expands on hover to fit text
     },
   },
   sidebarNav: {
-    listStyleType: 'none',
+    listStyleType: "none",
     margin: 0, // Resets default margin
     padding: 0, // Resets default padding
   },
   navItem: {
-    display: 'flex', // Corrected the typo here
-    alignItems: 'center',
-    padding: '10px 0', // Consistent padding for top and bottom
+    display: "flex", // Corrected the typo here
+    alignItems: "center",
+    padding: "10px 0", // Consistent padding for top and bottom
   },
   navLink: {
-    display: 'flex',
-    alignItems: 'center',
-    textDecoration: 'none',
-    color: 'rgb(0, 0, 0)',
-    padding: '0 10px', // Padding inside the link for spacing
+    display: "flex",
+    alignItems: "center",
+    textDecoration: "none",
+    color: "rgb(0, 0, 0)",
+    padding: "0 10px", // Padding inside the link for spacing
   },
   icon: {
     marginRight: 10,
     width: 24,
-    height: 'auto',
+    height: "auto",
   },
   toolText: {
     opacity: 0,
-    transition: 'opacity 0.3s ease-in-out',
-    whiteSpace: 'nowrap',
+    transition: "opacity 0.3s ease-in-out",
+    whiteSpace: "nowrap",
   },
   sidebarHover: {
-    '&:hover $toolText': {
+    "&:hover $toolText": {
       opacity: 1,
     },
   },
@@ -61,19 +61,31 @@ export default function Sidebar() {
         <ul className={classes.sidebarNav}>
           <li className={classes.navItem}>
             <a href="#tool" className={classes.navLink}>
-              <img src="/paraphrase.png" alt="Tool Icon" className={classes.icon} />
+              <img
+                src="/paraphrase.png"
+                alt="Tool Icon"
+                className={classes.icon}
+              />
               <span className={classes.toolText}>Paraphrase</span>
             </a>
           </li>
           <li className={classes.navItem}>
             <a href="#tool" className={classes.navLink}>
-              <img src="/summary.png" alt="Tool Icon" className={classes.icon} />
+              <img
+                src="/summary.png"
+                alt="Tool Icon"
+                className={classes.icon}
+              />
               <span className={classes.toolText}>Summary</span>
             </a>
           </li>
           <li className={classes.navItem}>
             <a href="#tool" className={classes.navLink}>
-              <img src="/infograph.png" alt="Tool Icon" className={classes.icon} />
+              <img
+                src="/infograph.png"
+                alt="Tool Icon"
+                className={classes.icon}
+              />
               <span className={classes.toolText}>Infogrph</span>
             </a>
           </li>
